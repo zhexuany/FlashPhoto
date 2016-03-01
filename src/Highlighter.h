48311429d97f28b6f16b1fdabfc373d87629352d
zhexuany <yang4442@umn.edu>
2016-02-29 19:36:42 -0600

    undo

---
+++ src/Highlighter.h
@@ -0,0 +1,14 @@
+#ifndef HIGHLIGHTER_H
+#define  HIGHLIGHTER_H
+
+#include "Drawtool.h"
+#include "PixelBuffer.h"
+#include "ColorData.h"
+class Highlighter : public DrawTool {
+ public:
+  Highlighter(ColorData* toolColor, int width, int height);
+  ~Highlighter();
+  void fillInfluence();
+  void applyInfluence(int x, int y, PixelBuffer* buffer);
+};
+#endif
---
+++ src/Mask.h
@@ -0,0 +1,16 @@
+#ifndef MASK_H
+#define MASK_H
+/*This Mask class store a 2D array of floats */
+class Mask{
+public:
+    Mask(int w, int h);
+    virtual ~Mask();
+    int getHeight() const;
+    int getWidth() const;
+    float** getInfluence() const;
+protected:
+    int height;
+    int width;
+    float** influence;
+};
+#endif

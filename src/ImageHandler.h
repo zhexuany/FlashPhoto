8be54f8ca03bf0eddee2aaba0d96aaf3447ce6b7
jasper <reyno511@umn.edu>
2016-03-30 16:22:26 -0500

    refactoring

--- src/ImageHandler.h
+++ src/ImageHandler.h
@@ -7,15 +7,22 @@
 #include <png.h>
 #include <jpeglib.h>
 #include <setjmp.h>
+#include <string>
 
 class ImageHandler  {
  public :
   ImageHandler();
   ~ImageHandler();
-  PixelBuffer* loadpng(FILE *fp, long &Height, long &Width);
-  PixelBuffer* loadjpg(FILE *infile, long &Height, long &Width);
+  PixelBuffer* loadpng(FILE *fp, int &height, int &width);
+  PixelBuffer* loadjpg(FILE *infile, int &height, int &width);
+  PixelBuffer* loadimage(const std::string & filename, int &height, int &width);
   void savepng(FILE* file, int height, int width, PixelBuffer *buffer);
   void savejpg(FILE* infile, int height, int width, PixelBuffer *buffer);
+  void saveimage(const std::string & filename, int height, int width, PixelBuffer *buffer);
+    
+ private:
+  bool isjpeg(const std::string & name);
+  bool hasSuffix(const std::string & str, const std::string & suffix);
 };
 
 #endif // IMAGEHANDLER_H

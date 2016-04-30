ab0eaffd5d41436ac8cc788b1db7387fc857862f
zhexuany <yang4442@umn.edu>
2016-04-29 22:43:48 -0500

    polish all make files

--- test/src/FilterToolTest.h
+++ test/src/FilterToolTest.h
@@ -1,19 +1,5 @@
 #include <cxxtest/TestSuite.h>
-#include "PixelBuffer.h"
-#include "ColorData.h"
-#include "FEdgeDetection.h"
-#include "FSaturation.h"
-#include "FThreshold.h"
-#include "FilterFactory.h"
-#include "FBlur.h"
-#include "FMotionBlur.h"
-#include "FSharpen.h"
-#include "FChannel.h"
-#include "FQuantize.h"
-#include "FSpecial.h"
-#include "Filter.h"
-#include "ImageHandler.h"
-
+#include <libphoto.h>
 
 class FilterToolTest : public CxxTest::TestSuite {
   public:

e0bed1dc8a1500134320975baa4644fa4b70a4be
zhexuany <yang4442@umn.edu>
2016-04-29 21:44:32 -0500

    finished basic set-up for interactive tool.

--- test/src/InteractiveToolTest.h
+++ test/src/InteractiveToolTest.h
@@ -2,9 +2,21 @@
 #define __PENTEST_H
 
 #include <cxxtest/TestSuite.h>
-#include <libphoto/Pen.h>
-#include <libphoto/ColorData.h>
-#include <libphoto/Mask.h>
+#include "PixelBuffer.h"
+#include "ColorData.h"
+#include "DrawTool.h"
+#include "Pen.h"
+#include "Eraser.h"
+#include "Mask.h"
+#include "SprayCan.h"
+#include "Blur.h"
+#include "Crayon.h"
+#include "FillTool.h"
+#include "Highlighter.h"
+#include "Stamp.h"
+#include "CalligraphyPen.h"
+#include "WaterColor.h"
+
 //
 // A simple test suite: Just inherit CxxTest::TestSuite and write tests!
 //
@@ -12,45 +24,89 @@
 
 class InteractiveToolTest : public CxxTest::TestSuite {
   PixelBuffer *buffer;
-  DrawTool *tool;
+  ColorData* color;
+  const Mask * mask;
+  int size;
 public:
   //declare a temporary Pixel buffer for testing
   void setUp(){
+    size = 30;
     buffer = new PixelBuffer(50, 50, ColorData(0, 0, 0));
+    color = new ColorData(0, 0, 0);
   }
+
   //delete buffer
   void tearDown(){
     delete buffer;
   }
 
-  void testTool(){
-    TS_TRACE("Starting Pen test");
-    ColorData toolColor(1, 1, 1);
-    Pen pen(toolColor, 5);
-    ColorData resColor = pen.getToolColor();
-    TS_TRACE("Starting Pen's tool color");
-    TS_ASSERT_EQUALS(toolColor.getRed(), res.getRed());
-    TS_ASSERT_EQUALS(toolColor.getBlue(), res.getBlue());
-    TS_ASSERT_EQUALS(toolColor.getGreen(), res.getGreen());
-    TS_TRACE("Finishing Pen's tool color");
-    Mask mask = pen.getMask();
-    TS_ASSERT()
-    TS_TRACE("Finishing Pen test");
+  void testBlur(){
+    DrawTool* blur = new Blur(30);
+    mask = blur -> getMask();
+    TS_ASSERT_EQUALS(mask -> getHeight(), 30);
   }
 
-  void testMask(Mask mask, int length, int height){
-    TS_TRACE("Starting to Mask Test");
-    TS_TRACE("Starting to Mask Height Test");
-    TS_ASSERT_EQUALS(dst_mask.getHeight(), height);
-    TS_TRACE("Finishing to Mask Height Test");
-    TS_TRACE("Starting to Mask Width Test");
-    TS_ASSERT_EQUALS(dst_mask.getWidth(), width);
-    TS_TRACE("Finishing to Mask Width Test");
-    TS_TRACE("Starting to Mask Value Test");
-    for(int i = 0; i < )
-    TS_TRACE("Finishing to Mask Value Test");
-    TS_TRACE("Starting to Mask Test");
+  void testStamp(){
+    //PixelBuffer* init = new PixelBuffer(0, 0, *color);
+    //DrawTool* stamp = new Stamp(init, 0, 0);
   }
 
+  void testCrayon(){
+    DrawTool* crayon = new Crayon(color, 20);
+    mask = crayon -> getMask();
+    TS_ASSERT_EQUALS(mask -> getHeight(), 20);
+  }
+  void testFillTool(){
+    DrawTool* fillTool = new FillTool(color, 50, 50);
+    mask = fillTool -> getMask();
+    TS_ASSERT_EQUALS(mask -> getHeight(), 50);
+  }
+
+  void testPen(){
+    DrawTool* pen = new Pen(color, 3);
+    mask = pen -> getMask();
+    TS_ASSERT_EQUALS(mask -> getHeight(), 3);
+  }
+
+  void testSparyCan(){
+    DrawTool* sparyCan = new SprayCan(color, 41);
+    mask = sparyCan -> getMask();
+    TS_ASSERT_EQUALS(mask -> getHeight(), 41);
+  }
+
+  void testEraser(){
+    DrawTool* eraser = new Eraser(21);
+    mask = eraser -> getMask();
+    TS_ASSERT_EQUALS(mask -> getHeight(), 21);
+  }
+
+  void testCalligraphyPen(){
+    DrawTool* calligraphyPen = new CalligraphyPen(color, 5, 15);
+    mask = calligraphyPen -> getMask();
+    TS_ASSERT_EQUALS(mask -> getHeight(), 15);
+    TS_ASSERT_EQUALS(mask -> getWidth(), 5);
+  }
+
+  void testHightlighter(){
+    DrawTool* highlighter = new Highlighter(color, 5, 15);
+    mask = highlighter -> getMask();
+    TS_ASSERT_EQUALS(mask -> getHeight(), 15);
+    TS_ASSERT_EQUALS(mask -> getWidth(), 5);
+  }
+
+  void testWaterColor(){
+    DrawTool* waterColor = new WaterColor(color, 21);
+    mask = waterColor -> getMask();
+    TS_ASSERT_EQUALS(mask -> getHeight(), 21);
+  }
+
+  void testMask(){
+  }
+
+  void testColorData(){
+    TS_ASSERT_EQUALS(color -> getRed(), 0);
+    TS_ASSERT_EQUALS(color -> getBlue(), 0);
+    TS_ASSERT_EQUALS(color -> getGreen(), 0);
+  }
 };
 #endif // __PENTEST_H

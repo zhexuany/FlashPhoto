b5f5c4efd57ba25fec608486ce5a599c90c0bbe2
zhexuany <yang4442@umn.edu>
2016-05-01 02:26:28 -0500

    finished all interactive test

--- test/src/InteractiveToolTest.h
+++ test/src/InteractiveToolTest.h
@@ -10,27 +10,22 @@
 
 
 class InteractiveToolTest : public CxxTest::TestSuite {
-  PixelBuffer *buffer;
   ColorData* backgroundColor;
-  ColorData* color;
+  ColorData* toolColor;
   const Mask * mask;
   double _delta;
-  int size;
 public:
   //declare a temporary Pixel buffer for testing
   void setUp(){
-    _delta = 0.001;
-    size = 30;
+    _delta = 0.01;
     backgroundColor = new ColorData(1, 1, 0.9);
-    buffer = new PixelBuffer(50, 50, ColorData(1, 1, 0.9));
-    color = new ColorData(0, 0, 0);
+    toolColor = new ColorData(0, 0, 0);
   }
 
   //delete buffer
   void tearDown(){
-    delete buffer;
     delete backgroundColor;
-    delete color;
+    delete toolColor;
   }
 
   void testBlur(){
@@ -45,71 +40,125 @@ public:
   }
 
   void testCrayon(){
-    DrawTool* crayon = new Crayon(backgroundColor, 20);
+    PixelBuffer *buffer = new PixelBuffer(800, 800, ColorData(1, 1, 0.9));
+    DrawTool* crayon = new Crayon(toolColor, 20);
     mask = crayon -> getMask();
     TS_ASSERT_EQUALS(mask -> getHeight(), 20);
+    crayon -> applyInfluence(25, 25, buffer);
+    testBuffer(25, 25, buffer);
+    delete buffer;
   }
 
   void testFillTool(){
-    DrawTool* fillTool = new FillTool(backgroundColor, 50, 50);
+    PixelBuffer *buffer = new PixelBuffer(800, 800, ColorData(1, 1, 0.9));
+    DrawTool* fillTool = new FillTool(toolColor, 800, 800);
     mask = fillTool -> getMask();
-    TS_ASSERT_EQUALS(mask -> getHeight(), 50);
+    TS_ASSERT_EQUALS(mask -> getHeight(), 800);
+    fillTool -> applyInfluence(25, 25, buffer);
+    for(int i = 0; i < buffer -> getWidth(); i++){
+      for(int j = 0; j < buffer -> getHeight(); j++){
+        testColorData(*toolColor, buffer -> getPixel(i, j));
+      }
+    }
+    delete buffer;
   }
 
   void testPen(){
+    PixelBuffer *buffer = new PixelBuffer(800, 800, ColorData(1, 1, 0.9));
     //default backgroundColor data is (0, 0, 0)
-    DrawTool* pen = new Pen(color, 3);
+    DrawTool* pen = new Pen(toolColor, 3);
     mask = pen -> getMask();
     //pen -> printfInfluence();
-    float** influence = mask -> getInfluence();
     TS_ASSERT_EQUALS(mask -> getHeight(), 3);
     // apply mask at 25 25 with radius 3
     pen -> applyInfluence(25, 25, buffer);
-    for(int i = 0; i < mask -> getWidth()/2; i++){
-      for(int j = 0; j < mask -> getHeight()/2; j++){
-        ColorData currentColor = buffer -> getPixel(25 + i, 25 + j);
-        ColorData newColor =((*color)*influence[i][j])
-          + currentColor*(1 - influence[i][j]);
-        testColorData(currentColor, newColor);
-      }
-    }
+    testBuffer(25, 25, buffer);
+    delete buffer;
   }
 
   void testSparyCan(){
-    DrawTool* sparyCan = new SprayCan(backgroundColor, 41);
+    PixelBuffer *buffer = new PixelBuffer(800, 800, ColorData(1, 1, 0.9));
+    int width = 41;
+    DrawTool* sparyCan = new SprayCan(toolColor, width);
     mask = sparyCan -> getMask();
-    TS_ASSERT_EQUALS(mask -> getHeight(), 41);
+    TS_ASSERT_EQUALS(mask -> getHeight(), width);
+    sparyCan -> applyInfluence(25, 25, buffer);
+    testBuffer(25, 25, buffer);
+    delete buffer;
   }
 
   void testEraser(){
+    PixelBuffer *buffer = new PixelBuffer(800, 800, ColorData(1, 1, 0.9));
     DrawTool* eraser = new Eraser(21);
     mask = eraser -> getMask();
     TS_ASSERT_EQUALS(mask -> getHeight(), 21);
+    eraser -> applyInfluence(25, 25, buffer);
+    int height = mask-> getHeight();
+    int width = mask -> getWidth();
+    int bufferHeight = buffer -> getHeight();
+    int x = - width/2 + 25;
+    int y = bufferHeight - 25 - height/2;
+    float ** influence = mask -> getInfluence();
+    for(int i =0; i < mask -> getWidth(); i++){
+      for(int j = 0; j < mask -> getHeight(); j++){
+        if(influence[i][j] == 1.0) {
+          testColorData(*backgroundColor, buffer -> getPixel(x + i, y + j));
+        }
+      }
+    }
+    delete buffer;
   }
 
   void testCalligraphyPen(){
-    DrawTool* calligraphyPen = new CalligraphyPen(backgroundColor, 5, 15);
+    PixelBuffer *buffer = new PixelBuffer(800, 800, ColorData(1, 1, 0.9));
+    DrawTool* calligraphyPen = new CalligraphyPen(toolColor, 5, 15);
     mask = calligraphyPen -> getMask();
     TS_ASSERT_EQUALS(mask -> getHeight(), 15);
     TS_ASSERT_EQUALS(mask -> getWidth(), 5);
+    calligraphyPen -> applyInfluence(25, 25, buffer);
+    testBuffer(25, 25, buffer);
+    delete buffer;
   }
 
   void testHightlighter(){
-    DrawTool* highlighter = new Highlighter(backgroundColor, 5, 15);
+    PixelBuffer *buffer = new PixelBuffer(800, 800, ColorData(1, 1, 0.9));
+    DrawTool* highlighter = new Highlighter(toolColor, 5, 15);
     mask = highlighter -> getMask();
     TS_ASSERT_EQUALS(mask -> getHeight(), 15);
     TS_ASSERT_EQUALS(mask -> getWidth(), 5);
+    highlighter -> applyInfluence(25, 25, buffer);
+    testBuffer(25, 25, buffer);
+    delete buffer;
   }
 
   void testWaterColor(){
-    DrawTool* waterColor = new WaterColor(backgroundColor, 21);
+    PixelBuffer *buffer = new PixelBuffer(800, 800, ColorData(1, 1, 0.9));
+    DrawTool* waterColor = new WaterColor(toolColor, 21);
     mask = waterColor -> getMask();
     TS_ASSERT_EQUALS(mask -> getHeight(), 21);
+    waterColor -> applyInfluence(25, 25, buffer);
+    testBuffer(25, 25, buffer);
+    delete buffer;
   }
 
-  void testMask(){
-  }
+  void testBuffer(int x, int y, PixelBuffer* buffer){
+    int height = mask-> getHeight();
+    int width = mask -> getWidth();
+    int bufferHeight = buffer -> getHeight();
+    x -= width/2;
+    y = bufferHeight - y - height/2;
+    float ** influence = mask -> getInfluence();
+    // printfInfluence();
+    for(int i = 0; i < width; i++){
+      for(int j = 0; j < height; j++){
+        ColorData newColor =((*toolColor)*influence[i][j])
+          + *backgroundColor*(1 - influence[i][j]);
+        ColorData currentColor = buffer -> getPixel(x+i, y+j);
+        testColorData(newColor, currentColor);
+      }
+    }
 
+  }
   void testColorData(ColorData src, ColorData dst){
     TS_ASSERT_DELTA( src.getRed(), dst.getRed(), _delta);
     TS_ASSERT_DELTA( src.getGreen(), dst.getGreen(), _delta);

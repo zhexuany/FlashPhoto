16009e5ee7b18767edf41817821fe40d5ee0cac6
jasper <reyno511@umn.edu>
2016-05-04 22:33:52 -0500

    updating member comments

--- doxyOutput/html/search/variables_0.js
+++ doxyOutput/html/search/variables_0.js
@@ -1,5 +1,11 @@
 var searchData=
 [
-  ['active',['active',['../classGLUI__Control.html#ad285387e771b46fb294935f68e967a77',1,'GLUI_Control']]],
-  ['active_5ftype',['active_type',['../classGLUI__Control.html#ab9864db034526ddd5bbff94a5fb3ee9b',1,'GLUI_Control']]]
+  ['m_5fcurfilter',['m_curFilter',['../classFlashPhotoApp.html#a584dd16d8f2aad51028759a1dd94c83f',1,'FlashPhotoApp']]],
+  ['m_5fcurtool',['m_curTool',['../classFlashPhotoApp.html#a4fe8a885ae7b5e698c40a7585b4e292d',1,'FlashPhotoApp']]],
+  ['m_5fprevx',['m_prevX',['../classFlashPhotoApp.html#a7552f6cf6441c53d020e5a0124031bcb',1,'FlashPhotoApp']]],
+  ['m_5fprevy',['m_prevY',['../classFlashPhotoApp.html#a6a9759bdfbdf16bf0091f1694604a64a',1,'FlashPhotoApp']]],
+  ['m_5fqueuesize',['m_queueSize',['../classFlashPhotoApp.html#ae45bc3761686cbb4d5f9c01b048816c0',1,'FlashPhotoApp']]],
+  ['m_5fstampheight',['m_stampHeight',['../classFlashPhotoApp.html#a2e397b37bfb3ea981efc795b7a2a9e9e',1,'FlashPhotoApp']]],
+  ['m_5fstampwidth',['m_stampWidth',['../classFlashPhotoApp.html#a7dab8bf3eb60e4c99310660c9c32f3a5',1,'FlashPhotoApp']]],
+  ['m_5ftool',['m_tool',['../classFlashPhotoApp.html#a69d8dcf7147882964a6d2832bbab67e2',1,'FlashPhotoApp']]]
 ];
